41c1966ebe53453d6f37bc24d1dc315193466c82
bandito <bandito@george-chatzigeorgious-macbook.local>
2009-11-01 15:38:41 +0200

    Added migration and removed the empty task

--- migrations/base.rb
+++ migrations/base.rb
@@ -1,16 +1,18 @@
 class Base < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
-      t.column :parent_id, :integer, :limit => 20, :null => true
+      t.column :parent_id, :integer, :limit => 2, :null => true
       t.column :id_path, :string, :limit => 200, :null => true
+      t.column :level, :string, :integer, :limit => 1, :null => true
+      t.column :children_count, :string, :integer, :limit => 2, :null => true
 
       #optional
-      t.column :level, :string, :integer, :limit => 1, :null => true
-      t.column :branch_ids, :string, :limit => 255, :null => true
-      t.column :top_level_parent_id, :string, :limit => 200, :null => true
-      t.column :children_ids, :string, :limit => 255, :null => true
+      t.column :family_id, :integer, :limit => 2, :null => true
 
     end
+
+add_index :categories, :parent_id
+add_index :categories, :id_path
   end
 
   def self.down
